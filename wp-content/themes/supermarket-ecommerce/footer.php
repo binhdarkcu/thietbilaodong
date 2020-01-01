bd50fdeecc44a167721bf8da79eabb0ed3ab60a6
Binh Lam <binhdarkcu@gmail.com>
2020-01-01 22:37:55 +0700

    aaa

--- wp-content/themes/supermarket-ecommerce/footer.php
+++ wp-content/themes/supermarket-ecommerce/footer.php
@@ -10,12 +10,13 @@
 ?>
 		</div>
 		<?php get_template_part('tpl', 'support');?>
-		<footer id="colophon" class="site-footer" role="contentinfo">
+		<footer class="footer-wrapper" role="contentinfo">
+			<div class="block-top d-none d-md-block"><div class="container"><div class="block-container row middle-xs between-xs"><div class="block-wrapper top-xs tappable"><div class="content-img"> <svg class="icon" viewBox="0 0 47.72 48"><path d="m47.54 4.96c-15.05.44-23.68-4.92-23.68-4.92s-8.63 5.36-23.68 4.92c0 0-1.46 17.33 4 28.48a25.9 25.9 0 005.81 6.93 72.37 72.37 0 0013.87 7.67 72.37 72.37 0 0013.84-7.64 25.9 25.9 0 005.81-6.93c5.5-11.19 4.03-28.51 4.03-28.51zm-8.06 18.34-14.44-7.6c-2.4-.85-3.16 1-3.16 1-1.06 2.62 2.19 3.86 2.19 3.86l14.2 7.36c-.23.65-.47 1.28-.75 1.88a16.54 16.54 0 01-3.66 4.67 35.07 35.07 0 01-10 5.85 36 36 0 01-10-5.78 16 16 0 01-3.68-4.65 24.66 24.66 0 01-1.46-4.26l12.84 6.71c2.4.87 3.25-.84 3.25-.84 1.18-2.37-2.07-3.62-2.07-3.62l-15-7.72a61.75 61.75 0 01-.38-8.81c10.5.28 16.5-3.61 16.5-3.61s6 3.89 16.48 3.61a60 60 0 01-.86 11.95z"></path></svg></div><div class="content tappable"><div class="title">Friendstore Đảm Bảo</div><div class="detail">Nhận hàng, hoặc được hoàn lại tiền.</div></div></div><div class="block-wrapper top-xs"><div class="content-img"> <svg class="icon" viewBox="0 0 44.94 48"><path d="m13.9 37.68a1.18 1.18 0 01-.64-.95 1 1 0 01 .94-1 1.54 1.54 0 01 .84.33l14.75 8.82 4.81-2.7-14.45-8.5a1.17 1.17 0 01-.76-1 1 1 0 011.69-.77l15.39 9.21 4.82-2.71-15-8.83c-.39-.24-.76-.51-.76-1a1 1 0 011.69-.77l15.85 9.48a6.54 6.54 0 001.88-5.08v-.87l-19.19-11.48a5.63 5.63 0 00-5.23.32l-7.7 4.48a5.26 5.26 0 01-5.54 0l-2.46-1.6s-.47-.35 0-.64l15.39-9.27a5.87 5.87 0 015.85 0l7.4 3.66a7.55 7.55 0 006.47-.52l4.89-2.94a5.24 5.24 0 00-2.6-3.48l-16.12-8.78a6.43 6.43 0 00-5.95-.64l-17.83 9.91s-2.33.82-2.33 4.76v17.61s-.12 3.87 2.7 5.4l15.77 8.68a7.1 7.1 0 007.26.32l2.13-1.2z" fill-rule="evenodd"></path></svg></div><div class="content"><div class="title">NGƯỜI BÁN UY TÍN</div><div class="detail">Vui lòng xem đánh giá của Shop và liên hệ chủ shop để được giải đáp thắc mắc.</div></div></div><div class="block-wrapper top-xs"><div class="content-img"> <svg class="icon" enable-background="new 0 0 48 48" viewBox="0 0 48 48" x="0" y="0"><path d="m42.4 14.3c.1-4.3-1.5-6.3-1.5-6.3s-3.6-4.5-5.4-6.2c-1.5-1.5-3.3-1.7-3.9-1.8-.2 0-.3 0-.3 0h-6.9-6.9s-.1 0-.3 0c-.7.1-2.5.3-3.9 1.8-1.7 1.8-5.3 6.2-5.3 6.2s-1.5 2-1.5 6.2v6.6l35.9-6.3s0-.1 0-.2zm-17.9-2.3c-2.1 0-3.8-1.7-3.8-3.8s1.7-3.8 3.8-3.8 3.8 1.7 3.8 3.8c-.1 2.1-1.7 3.8-3.8 3.8zm14.3 35.9c-4.3.1-14.3.1-14.3.1v-1.7 1.7s-10 0-14.4-.1c-3.9-.3-3.6-2.7-3.6-2.7s0-2.7 0-6.6l35.9-6.3v12.9s.3 2.4-3.6 2.7zm-34.3-24.1 9-1.6v3.6l-4.6.8v1.5l4.6-.8v3.5l-4.6.8v4.4l-4.4.8zm17.8 4.5 1.8 4.8-3.9.8-1.4-4.4-.4.1v.2 4.5l-3.7.7v-13l4.7-.8c.6-.1 1.2-.1 1.7-.1.5.1 1 .2 1.4.5.4.2.7.6 1 1s .4.9.5 1.4c.2 1 .1 1.9-.2 2.7-.3.7-.8 1.2-1.5 1.6zm-1.7-3.5c-.1-.4-.2-.6-.5-.7s-.6-.1-1.1 0l-.8.1v2.2l.8-.1c.5-.1 1-.3 1.2-.5.4-.3.5-.6.4-1zm13.6-2.8v-3.6l-9.2 1.6v13l9.2-1.6v-3.6l-4.9 1v-1.4l4.9-.8v-3.3l-4.9.8v-1.3zm10.2-1.8v-3.6l-9.2 1.6v13l9.2-1.6v-3.6l-4.9 1v-1.4l4.9-.8v-3.3l-4.9.8v-1.3z"></path></svg></div><div class="content"><div class="title">MIỄN PHÍ CHO TẤT CẢ</div><div class="detail">Miễn phí tải về, miễn phí niêm yết sản phẩm, không phí hoa hồng. Tận hưởng Friendstore hoàn toàn miễn phí!</div></div></div></div></div></div>
 			<div class="container">
 				<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
 			</div>
 			<div class="clearfix"></div>
-			<div class="copyright"> 
+			<div class="copyright">
 				<div class="container">
 					<?php get_template_part( 'template-parts/footer/site', 'info' ); ?>
 				</div>
@@ -23,7 +24,7 @@
 		</footer>
 	</div>
 </div>
-		
+
 <?php wp_footer(); ?>
 
 </body>
--- wp-content/themes/supermarket-ecommerce/template-parts/page/content-page.php
+++ wp-content/themes/supermarket-ecommerce/template-parts/page/content-page.php
@@ -17,66 +17,6 @@
 	<div class="entry-content">
 		<?php if (class_exists('woocommerce') && is_front_page()) {?>
     <div class="row">
-			<div class="col-lg-3 col-md-4">
-				<button class="product-btn"><i class="fa fa-bars" aria-hidden="true"></i><?php echo esc_html_e('Danh mục', 'supermarket-ecommerce'); ?></button>
-				<div class="menu-categories">
-				<?php
-          $args = array(
-              //'number'     => $number,
-              'orderby' => 'title',
-              'order' => 'ASC',
-              'hide_empty' => 0,
-              'parent' => 18,
-              //'include'    => $ids
-          );
-          $product_categories = get_terms('product_cat', $args);
-          $count = count($product_categories);
-          if ($count > 0) {
-            foreach ($product_categories as $product_category) {
-              $product_cat_id = $product_category->term_id;
-              $cat_link = get_category_link($product_cat_id);
-        ?>
-          <div class="menu-category">
-            <a class="menu-link-hover" href="<?php echo esc_url(get_term_link($product_category)); ?>">
-              <i class="fas fa-angle-double-right"></i>
-              <?php echo esc_html($product_category->name); ?>
-            </a>
-            <div class="menu-div-show">
-              <?php
-                // $argProduct = array(
-                //   'orderby' => 'title',
-                //   'order' => 'ASC',
-                //   'hide_empty' => 0,
-                //   'parent' => $product_cat_id,
-                // );
-                // $products = get_terms('product', $argProduct);
-                // // $countProducts = count($products);
-                // echo esc_html($products->name);
-                $args = array(
-                  'category' => array( $product_category->name ),
-                  'orderby'  => 'name',
-              );
-              $products = wc_get_products( $args );
-              $countProducts = count($products);
-              if ($products) {
-                foreach ($products as $product) {
-                  # code...
-                  ?>
-                    <a style="color: black" class="menu-link-hover" href="<?php echo esc_url(get_term_link($product->get_slug())); ?>">
-                      <i class="fas fa-angle-double-right"></i>
-                      <?php echo esc_html($product->get_name()); ?>
-                    </a>
-                <?php }
-              }
-              ?>
-            </div>
-          </div>
-				<?php
-            }
-          }
-        ?>
-				</div>
-			</div>
       <div class="col-lg-9 col-md-8">
         <?php echo do_shortcode('[smartslider3 slider=2]'); ?>
       </div>
