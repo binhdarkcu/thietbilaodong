0f73510e2750662e93cbd1264852f07389b11312
baoch <baoch2310@gmail.com>
2020-01-02 10:44:18 +0700

    [UPDATE] slider
    - Remove smart slider
    - Added ml-slider

--- wp-content/themes/supermarket-ecommerce/custom-html/home-page-banner.php
+++ wp-content/themes/supermarket-ecommerce/custom-html/home-page-banner.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * Template part for displaying page content in homepage.php
+ * @package WordPress
+ * @subpackage supermarket-ecommerce
+ * @since 1.0
+ * @version 0.1
+ */
+
+?>
+
 <div data-vc-full-width="true" data-vc-full-width-init="true" data-vc-stretch-content="true" class="vc_row wpb_row vc_row-fluid vc_row-no-padding"
   style="position: relative; left: -345px; box-sizing: border-box; width: 1920px;">
   <div class="wpb_column vc_column_container vc_col-sm-12">
@@ -7,100 +18,10 @@
           <div class="home-banners-wrapper">
             <div class="container">
               <div class="home-banners">
-                <div class="main-banner light-background">
-                  <div id="rev_slider_1_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-source="gallery"
-                    style="margin: 0px auto; background: rgb(232, 232, 232); padding: 0px; height: 235px; overflow: visible;">
-                    <div id="rev_slider_1_1" class="rev_slider fullwidthabanner revslider-initialised tp-simpleresponsive rev_redraw_on_blurfocus"
-                      style="margin-top: 0px; margin-bottom: 0px; height: 235px;" data-version="5.4.8.3" data-slideactive="rs-6">
-                      <ul class="tp-revslider-mainul" style="visibility: visible; display: block; overflow: hidden; width: 797px; height: 100%; max-height: none; left: 0px;">
-                        <li data-index="rs-6" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"
-                          data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-1-100x50.png"
-                          data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3=""
-                          data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10=""
-                          data-description="" class="tp-revslider-slidesli active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 20; visibility: inherit; opacity: 1; background-color: rgba(255, 255, 255, 0);">
-                          <div class="slotholder" style="position: absolute; top: 0px; left: 0px; z-index: 0; width: 100%; height: 100%; visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
-                            <!--Runtime Modification - Img tag is Still Available for SEO Goals in Source - <img src="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-1.png" alt="" title="slider-banner-1.png" width="1242" height="373" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg defaultimg" data-no-retina="">-->
-                            <div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-1.png&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"
-                              src="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-1.png"></div>
-                          </div>
-                        </li>
-                        <li data-index="rs-7" data-transition="fade" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"
-                          data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-2-100x50.jpg"
-                          data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3=""
-                          data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10=""
-                          data-description="" class="tp-revslider-slidesli" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0; background-color: rgba(255, 255, 255, 0);">
-                          <div class="slotholder" style="position: absolute; top: 0px; left: 0px; z-index: 0; width: 100%; height: 100%; visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
-                            <!--Runtime Modification - Img tag is Still Available for SEO Goals in Source - <img src="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-2.jpg" alt="" title="slider-banner-2.jpg" width="1242" height="373" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg defaultimg" data-no-retina="">-->
-                            <div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-2.jpg&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"
-                              src="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-2.jpg"></div>
-                          </div>
-                        </li>
-                        <li data-index="rs-8" data-transition="fade" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"
-                          data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-3-100x50.jpg"
-                          data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3=""
-                          data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10=""
-                          data-description="" class="tp-revslider-slidesli" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0; background-color: rgba(255, 255, 255, 0);">
-                          <div class="slotholder" style="position: absolute; top: 0px; left: 0px; z-index: 0; width: 100%; height: 100%; visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
-                            <!--Runtime Modification - Img tag is Still Available for SEO Goals in Source - <img src="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-3.jpg" alt="" title="slider-banner-3.jpg" width="1242" height="373" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg defaultimg" data-no-retina="">-->
-                            <div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-3.jpg&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"
-                              src="https://store.vietfriend.info/wp-content/uploads/revslider/home-banners/slider-banner-3.jpg"></div>
-                          </div>
-                        </li>
-                        <li data-index="rs-10" data-transition="fade" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"
-                          data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-5-100x50.jpg"
-                          data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3=""
-                          data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10=""
-                          data-description="" class="tp-revslider-slidesli" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0; background-color: rgba(255, 255, 255, 0);">
-                          <div class="slotholder" style="position: absolute; top: 0px; left: 0px; z-index: 0; width: 100%; height: 100%; visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
-                            <!--Runtime Modification - Img tag is Still Available for SEO Goals in Source - <img src="https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-5.jpg" alt="" title="slider-banner-5" width="1242" height="373" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg defaultimg" data-no-retina="">-->
-                            <div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-5.jpg&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"
-                              src="https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-5.jpg"></div>
-                          </div>
-                        </li>
-                        <li data-index="rs-11" data-transition="fade" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"
-                          data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-6-100x50.jpg"
-                          data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3=""
-                          data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10=""
-                          data-description="" class="tp-revslider-slidesli" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0; background-color: rgba(255, 255, 255, 0);">
-                          <div class="slotholder" style="position: absolute; top: 0px; left: 0px; z-index: 0; width: 100%; height: 100%; visibility: inherit; opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">
-                            <!--Runtime Modification - Img tag is Still Available for SEO Goals in Source - <img src="https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-6.jpg" alt="" title="slider-banner-6" width="1242" height="373" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg defaultimg" data-no-retina="">-->
-                            <div class="tp-bgimg defaultimg " data-bgcolor="undefined" style="background-repeat: no-repeat; background-image: url(&quot;https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-6.jpg&quot;); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit; z-index: 20;"
-                              src="https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-6.jpg"></div>
-                          </div>
-                        </li>
-                      </ul>
-                      <div class="tp-bannertimer" style="height: 5px; background: rgba(0, 0, 0, 0.15); width: 0%; visibility: visible; transform: matrix(1, 0, 0, 1, 0, 0);"></div>
-                      <div class="tp-loader spinner3" style="display: none;">
-                        <div class="dot1"></div>
-                        <div class="dot2"></div>
-                        <div class="bounce1"></div>
-                        <div class="bounce2"></div>
-                        <div class="bounce3"></div>
-                      </div>
-                      <div class="tp-leftarrow tparrows uranus" style="top: 50%; transform: matrix(1, 0, 0, 1, 20, -25); left: 0px;"></div>
-                      <div class="tp-rightarrow tparrows uranus" style="top: 50%; transform: matrix(1, 0, 0, 1, -70, -25); left: 100%;"></div>
-                      <div class="tp-bullets uranus horizontal nav-pos-hor-center nav-pos-ver-bottom nav-dir-horizontal"
-                        style="width: 95px; height: 15px; top: 100%; transform: matrix(1, 0, 0, 1, -47, -35); left: 50%;">
-                        <div class="tp-bullet selected" style="left: 0px; top: 0px;">
-                          <span class="tp-bullet-inner"></span>
-                        </div>
-                        <div class="tp-bullet" style="left: 20px; top: 0px;">
-                          <span class="tp-bullet-inner"></span>
-                        </div>
-                        <div class="tp-bullet" style="left: 40px; top: 0px;">
-                          <span class="tp-bullet-inner"></span>
-                        </div>
-                        <div class="tp-bullet" style="left: 60px; top: 0px;">
-                          <span class="tp-bullet-inner"></span>
-                        </div>
-                        <div class="tp-bullet" style="left: 80px; top: 0px;">
-                          <span class="tp-bullet-inner"></span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                <div class="main-banner light-background" style="width: 69%">
+                  <?php echo do_shortcode('[metaslider id="662"]'); ?>
                 </div>
-                <div class="right-wrapper">
+                <div class="right-wrapper"  style="width: 30%">
                   <a href="#" class="right-banner" target="_blank" rel="noopener noreferrer">
                     <div class="full-height light-background">
                       <div class="right-banner-image" style="background-image: url(https://store.vietfriend.info/wp-content/uploads/2019/05/slider-banner-8.png);"></div>
